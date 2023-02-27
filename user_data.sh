6bb2e44826e13cb3a9af5727fe8df74a92524b01
Antonio Bezerra <antonio.bezerra1414@gmail.com>
2023-02-27 09:54:56 -0300

    Instalação do docker-compose

--- user_data.sh
+++ user_data.sh
@@ -6,3 +6,7 @@ sudo yum install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
+
+# Instalação do docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.15.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
